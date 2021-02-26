43ca4b6d26d1888be27d76596a8b317117d899a5
デニス <76629978+denisputraa@users.noreply.github.com>
2021-02-26 22:39:59 +0700

    Update help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -49,32 +49,10 @@ const donasi = (name) => {
 exports.donasi = donasi
 // iklan menu
 const menu = (name) => {
-	return `╭──────「 *_ᴀᴅɪᴛɪʏᴀ ꜱᴛᴏʀᴇ_* 」
+	return `╭──────「 *_ɪᴋʟᴀɴ_* 」
 ┴
-│🔰 _*UNCHEK GG BY ADITIYA STORE*_  🔰
-│
-│ *📧 20 UNCHEK : 10K*
-│ *📧 30 UNCHEK : 15K*
-│ *📧 40 UNCHEK : 20K*
-│ *📧 50 UNCHEK : 25K*
-│ *📧 55 UNCHEK : 30K*
-│ *📧 75 UNCHEK : 35K*
-│ _*DAN SETERUSNYA...*_
-│
-│ _*TAKE / BELI BANYAK KEUNTUNGAN HOKI LEBIH BESAR*_
-│
-│ *GAK DAPAT WE KASIH LAGI 1 ×*
-│
-│ *KHUSUS TANGGAL 24 - 26 , PEMBELIAN 30K DAPAT 200 UNCHEK*
-│
-│ *T E S T I ? ADA HAMPIR BEJIBUN + PUSH TESTI*
-│
-│ _*PAYMENT ?*_
-│ *- G O P A Y💸*
-│
-│https://chat.whatsapp.com/Kmn8Q3Az0CDFEOBJHPyBSh
-┬
-╰──────「 *_ɪᴋʟᴀɴ_* 」
+│your text
+╰──────「 *_ᴅɴsʙᴏᴛ_* 」
 `
 }
 exports.menu = menu
